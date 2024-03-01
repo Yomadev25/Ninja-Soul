2b52c43a3d33d298ca7180097bbaa46234645cc4
YomaDev25 <yogolf2011@gmail.com>
2024-03-01 15:32:02 +0700

    Add Genbu mobs

---
+++ Assets/Characters/Enemy/Genbu Knuckle II/Scripts/Genbu_Punch.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Genbu Punch", menuName = "Enemy/Genbu/Punch")]
+public class Genbu_Punch : EnemyCombo
+{
+    public override async void Execute(EnemyStateMachine context, EnemyCombatState state)
+    {
+        Transform target = context.GetCombatTarget();
+        int side = Random.Range(0, 2);
+        context.Anim.SetTrigger(side == 0 ? "PunchL" : "PunchR");
+
+        LeanTween.move(context.gameObject, context.transform.position + (target.position - context.transform.position) / 2f, 0.5f).setOnComplete(async () =>
+        {
+            await Task.Delay(1000);
+            state.OnAttacked();
+        });
+    }
+}
