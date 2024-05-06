1797e7c9f8025224fbc0f8c477026d1a00bbf6dc
YomaDev25 <yogolf2011@gmail.com>
2024-05-06 13:18:12 +0700

    Add cheat class

---
+++ Assets/Scripts/Cheat.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheat : Singleton<Cheat>
+{
+    public bool active;
+
+    //Heal
+    //Immortal
+    //Add damage
+    //Unlock all weapon
+    //Unlock all stage
+}
